3b531fe95e66d9b452aa9f2313e6648e6c136b5f
root <root@LAPTOP-R3SPKBB2>
2024-10-14 21:57:57 +0800

    finish phase3:2

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -385,9 +385,11 @@ Disassembly of section .text:
   400f43:	48 83 ec 18          	sub    $0x18,%rsp
   // lea 指令计算地址，将 rsp+0xc 和 rsp+0x8 的地址分别加载到 rcx 和 rdx，这些值将用于后续的函数调用
   // 在 sscanf 调用之后，解析出来的第一个值会存储在 0x8(%rsp) 处（与 %rdx 对应），第二个值会存储在 0xC(%rsp) 处（与 %rcx 对应）。
-  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi
+  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx // 此为 sscanf 的第四个参数
+  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx // 此为 sscanf 的第三个参数
+  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi // 此为 sscanf 的第二个参数（esi 约等于 rsi）
+                                                          // esi 是 rsi 的低 32 位，使用 esi 可以节约一点资源
+                                                          // sscanf 的第一个参数 rdi 在函数调用前就设置好了
   400f56:	b8 00 00 00 00       	mov    $0x0,%eax
   400f5b:	e8 90 fc ff ff       	call   400bf0 <__isoc99_sscanf@plt>
   // 检查 sscanf 的返回值，eax 存储了 sscanf 成功解析的数字个数。如果返回值大于 1，跳转到 0x400f6a 处继续执行；否则，调用 explode_bomb 函数，触发爆炸。
@@ -429,20 +431,24 @@ Disassembly of section .text:
   400fcd:	c3                   	ret    
 
 0000000000400fce <func4>:
-  400fce:	48 83 ec 08          	sub    $0x8,%rsp
-  400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
-  400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
+  // edx=0xe esi=0x0 edi=第一个输入数
+  400fce:	48 83 ec 08          	sub    $0x8,%rsp // 右 = 右 - 左
+  400fd2:	89 d0                	mov    %edx,%eax // 右 = 左， eax=0xe
+  400fd4:	29 f0                	sub    %esi,%eax // eax=0xe
+  400fd6:	89 c1                	mov    %eax,%ecx // ecx=0xe
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx // 右 = 右 >> 左，ecx=0（右移了 31 位）
+  400fdb:	01 c8                	add    %ecx,%eax // 右 = 右 + 左，eax=0xe
+  400fdd:	d1 f8                	sar    %eax // 算数右移一位，eax=0x7
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx // ecx = rax + rsi * 1 = 7
+
+  400fe2:	39 f9                	cmp    %edi,%ecx // 第一个输入数 <= 7 ？
   400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
+
   400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
   400fe9:	e8 e0 ff ff ff       	call   400fce <func4>
   400fee:	01 c0                	add    %eax,%eax
   400ff0:	eb 15                	jmp    401007 <func4+0x39>
+
   400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
   400ff7:	39 f9                	cmp    %edi,%ecx
   400ff9:	7d 0c                	jge    401007 <func4+0x39>
@@ -454,22 +460,29 @@ Disassembly of section .text:
 
 000000000040100c <phase_4>:
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
-  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  40101f:	b8 00 00 00 00       	mov    $0x0,%eax
+  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx // 设置第四个参数
+  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx // 设置第三个参数
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi // 设置第二个参数，值为 "%d %d"（gdb 里查出来的）
+                                                          // 第一个参数为输入值
+  40101f:	b8 00 00 00 00       	mov    $0x0,%eax      // eax 的值是 sscanf 的返回值，也就是输入的数个数
   401024:	e8 c7 fb ff ff       	call   400bf0 <__isoc99_sscanf@plt>
+  // 若输入的数不是两个则结束
   401029:	83 f8 02             	cmp    $0x2,%eax
-  40102c:	75 07                	jne    401035 <phase_4+0x29>
+  40102c:	75 07                	jne    401035 <phase_4+0x29> // 如果不等于，则跳转到指定地址
+  // 如果在上一步的比较中，0x8(%rsp)（第一个输入数） 的值小于或等于 0xe（14），则程序将跳转到地址 0x40103a
   40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
   401033:	76 05                	jbe    40103a <phase_4+0x2e>
   401035:	e8 00 04 00 00       	call   40143a <explode_bomb>
+  //
   40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
   40103f:	be 00 00 00 00       	mov    $0x0,%esi
   401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
+  // edx=0xe esi=0x0 edi=第一个输入数
   401048:	e8 81 ff ff ff       	call   400fce <func4>
+  // 若返回值不是 0 则 bomb
   40104d:	85 c0                	test   %eax,%eax
   40104f:	75 07                	jne    401058 <phase_4+0x4c>
+  // 如果 第二个输入数 的值不为零，则 bomb
   401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
   401056:	74 05                	je     40105d <phase_4+0x51>
   401058:	e8 dd 03 00 00       	call   40143a <explode_bomb>
